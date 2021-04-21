be34b8e609619bef7767b21c347944e3e64cbb9f
hjq <hjqi98@126.com>
2021-04-21 14:06:21 +0800

    springboot + knife4j + mybatisplus + mysql + shiro + log4j2

--- src/main/java/com/hjq/common/shiro/ShiroRealm.java
+++ src/main/java/com/hjq/common/shiro/ShiroRealm.java
@@ -6,10 +6,14 @@ import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationInfo;
 import org.apache.shiro.authc.AuthenticationToken;
 import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.Permission;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
 
 import javax.annotation.Resource;
+import javax.management.relation.Role;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ShiroRealm extends AuthorizingRealm {
 
@@ -18,10 +22,10 @@ public class ShiroRealm extends AuthorizingRealm {
     //授权
     @Override
     protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
-//        User userNew =(User) principalCollection.getPrimaryPrincipal();
-//        User user = service.findAllUserAndRoleAndPer(userNew.getName());
-//        List<String> stringRole=new ArrayList<>();
-//        List<String> stringPermission=new ArrayList<>();
+        User userNew =(User) principalCollection.getPrimaryPrincipal();
+        User user = service.findByLoginName(userNew.getLoginName());
+        List<String> stringRole=new ArrayList<>();
+        List<String> stringPermission=new ArrayList<>();
 //        for(Role role:user.getRoleList()){
 //            if(role!=null){
 //                stringRole.add(role.getName());
@@ -35,19 +39,19 @@ public class ShiroRealm extends AuthorizingRealm {
 //        SimpleAuthorizationInfo info=new SimpleAuthorizationInfo();
 //        info.addStringPermissions(stringPermission);
 //        info.addRoles(stringRole);
-//        return info;
+        return null;
 
     }
 
     //验证
     @Override
     protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
-//        User user = service.findByUsernameBasicInfo(username);
+//        User user = service.findByLoginName(loginName);
 //        if(user==null||user.getPassword()==null||"".equals(user.getPassword())){
 //            return null;
 //        }
 //        return new SimpleAuthenticationInfo(user, user.getPassword(), this.getClass().getName());
 //    }
-
+    return null;
     }
 }
