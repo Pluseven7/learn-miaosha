2fbc2d3d1dfc6c8590d45f57d7886553ec063eb6
hjq <hjqi98@126.com>
2021-04-25 12:35:09 +0800

    springboot + knife4j + mybatisplus + mysql + shiro + log4j2 2021年4月25日 整合完成

--- src/main/java/com/hjq/common/utils/UserSalt.java
+++ src/main/java/com/hjq/common/utils/UserSalt.java
@@ -2,13 +2,28 @@ package com.hjq.common.utils;
 
 
 
+import lombok.Data;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 /*
  * 生成私盐
  */
+@Data
 public class UserSalt {
-    List<String> arr[] = new ArrayList<String>["abcde"];
 
+    private String PublicSalt = "miaosha";
+
+    private StringBuffer PrivateSalt;
+
+    private String getSalt(int n){
+        char[] chars = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm0123456789!@#$%^&*()".toCharArray();
+        for(int i = 0; i < n; i++) {
+            char aChar = chars[new Random().nextInt(chars.length)];
+            PrivateSalt.append(aChar);
+        }
+        return PrivateSalt+PublicSalt;
+    }
 }
